e72da72c6b1ed1f452605b175373281efda9644e
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-12 14:08:09 -0600

    wrote SQL queries

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ require('dotenv').config();
 const app = express();
 let port = 3000;
 
+massive(process.env.CONNECTION_STRING)
+       .then(db => app.set('db', db));
+
 // middleware
 app.use(bodyParser.json());
 app.use(cors());
-
 
 // set up server on port 3000
 app.listen(port, console.log(`Listening on port ${port}`));
